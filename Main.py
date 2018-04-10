feb4be33b870ae0d5f3602fa4c65ab0e370c418a
jhavens <jhavens12@gmail.com>
2018-04-10 14:33:32 -0400

    working version

--- Main.py
+++ Main.py
@@ -10,8 +10,6 @@ amazon = AmazonAPI(credentials.access_key, credentials.secret_key, credentials.a
 joycon_price = 79
 game_price = 59
 
-joycon_dict = links.joycons.copy()
-
 def extract_source(url):
     headers = {"User-Agent":"Mozilla/5.0"}
     source=requests.get(url, headers=headers).text
@@ -23,12 +21,12 @@ def print_bby_price(item,website):
     soup = BeautifulSoup(page, 'html.parser')
     name_box = soup.find(attrs={'class': 'price-block priceblock-large'})
     price = name_box.get('data-customer-price')
-    if float(price) >= joycon_price and item == 'joycon':
+    if float(price) >= joycon_price and item == 'joycons':
         return
     if float(price) >= game_price and item == 'games':
         return
     else:
-        return("Best Buy: $"+str(price))
+        print("Best Buy: $"+str(price))
 
 def print_gs_price(item,website):
     #for gamestop
@@ -37,12 +35,12 @@ def print_gs_price(item,website):
     name_box = soup.find('h3', attrs={'class': 'ats-prodBuy-price'})
     price1 = name_box.span
     price = price1.text
-    if float(price) >= joycon_price and item == 'joycon':
+    if float(price) >= joycon_price and item == 'joycons':
         return
     if float(price) >= game_price and item == 'games':
         return
     else:
-        return("Gamestop: $"+str(price))
+        print("Gamestop: $"+str(price))
 
 def print_wal_price(item,website):
     #for walmart
@@ -50,12 +48,12 @@ def print_wal_price(item,website):
     soup = BeautifulSoup(page, 'html.parser')
     name_box = soup.find(attrs={'class': "Price-characteristic"})
     price = name_box.get('content')
-    if float(price) >= joycon_price and item == 'joycon':
+    if float(price) >= joycon_price and item == 'joycons':
         return
     if float(price) >= game_price and item == 'games':
         return
     else:
-        return("Walmart: $"+str(price))
+        print("Walmart: $"+str(price))
 
 def print_tar_price(item,website):
     #for target
@@ -65,60 +63,51 @@ def print_tar_price(item,website):
     price1 = name_box.span
     price2 = price1.text
     price = str(price2).replace('$','')
-    if float(price) >= joycon_price and item == 'joycon':
+    if float(price) >= joycon_price and item == 'joycons':
         return
     if float(price) >= game_price and item == 'games':
         return
     else:
-        return("Target: $"+str(price))
+        print("Target: $"+str(price))
 
 def print_amazon_price(item,website):
     product = amazon.lookup(ItemId=website)
     price1 = product.price_and_currency[0]
     price = str(price1).replace("Decimal('')",'')
-    if float(price) >= joycon_price and item == 'joycon':
+    if float(price) >= joycon_price and item == 'joycons':
         return
     if float(price) >= game_price and item == 'games':
         return
     else:
-        return("Amazon: $"+str(price))
+        print("Amazon: $"+str(price))
 
-def print_dictionary(dict):
-    print_dict = {}
-    for entry in dict: #need to give functions website
-        for link in dict[entry]['links']:
-            if entry == 'bby':
-                print_list['bby_price'] = print_bby_price(dict[entry]['type'],dict[entry]['links'][link]) #give 'joycons' and link dictionary
-            if entry == 'gs':
-                print_list['gs_price'] = print_gs_price(dict[entry]['type'],dict[entry]['links'][link])
-            if entry == 'wal':
-                print_list['wal_price'] = print_wal_price(dict[entry]['type'],dict[entry]['links'][link])
-            if entry == 'tar':
-                print_list['tar_price'] = print_tar_price(dict[entry]['type'],dict[entry]['links'][link])
-            if entry == 'amazon':
-                print_list['amazon_price'] = print_amazon_price(item,dict[entry])
-    if bby_price == None and gs_price == None and wal_price == None and tar_price == None and amazon_price == None:
-        return
-    else:
-        print(dict)
-        for price in print_dict:
-            if print_dict[price] != None:
-                print(price+": "+str(print_dict[price]))
+def print_dictionary(item,dict):
+    for entry in dict:
+        if entry == 'bby':
+            print_bby_price(item,dict[entry])
+        if entry == 'gs':
+            print_gs_price(item,dict[entry])
+        if entry == 'wal':
+            print_wal_price(item,dict[entry])
+        if entry == 'tar':
+            print_tar_price(item,dict[entry])
+        if entry == 'amazon':
+            print_amazon_price(item,dict[entry])
 
+print("Joycons:")
+print("********")
 
-# print("Joycons:")
-# print("********")
-
-for item in joycon_dict:
-    #print(item)
-    print_dictionary(joycon_dict[item])#['type'],joycon_dict[item]['links'])
+for item in links.joycons:
+    print(item)
+    type = 'joycons'
+    print_dictionary(type,links.joycons[item])
     print()
 
-# print("Games:")
-# print("******")
-#
-# for item in links.games:
-#     print(item)
-#     type = 'games'
-#     print_dictionary(type,links.games[item])
-#     print()
+print("Games:")
+print("******")
+
+for item in links.games:
+    print(item)
+    type = 'games'
+    print_dictionary(type,links.games[item])
+    print()
