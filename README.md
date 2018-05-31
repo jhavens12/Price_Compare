d634a6eea1f6efb4040f3bf6a7f394470a525814
jhavens <jhavens12@gmail.com>
2018-05-31 11:11:20 -0400

    updated links

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Price_Compare
 price comparison test
 Requires https://github.com/yoavaviram/python-amazon-simple-product-api
+pip3 install python-amazon-simple-product-api
 
 pythonista: pip install https://github.com/lionheart/bottlenose/archive/master.zip
--- links.py
+++ links.py
@@ -7,66 +7,66 @@ accessories['Joycon Charging Grip']['Walmart'] = 'https://www.walmart.com/ip/Nin
 accessories['Joycon Charging Grip']['Target'] = 'https://www.target.com/p/nintendo-174-switch-joy-con-153-charging-grip/-/A-52180893'
 accessories['Joycon Charging Grip']['Amazon'] = 'B01N33MFPK'
 
-joycons = {}
-#pink and green
-joycons['Joycons: Pink and Green'] = {}
-joycons['Joycons: Pink and Green']['msrp'] = 79.99
-joycons['Joycons: Pink and Green']['links'] = {}
-joycons['Joycons: Pink and Green']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-pink-neon-green/6182525.p?skuId=6182525'
-joycons['Joycons: Pink and Green']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-neon-pink-neon-green/157796'
-joycons['Joycons: Pink and Green']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Neon-Pink-and-Neon-Green-45496881900/656821586'
-joycons['Joycons: Pink and Green']['links']['Target'] = 'https://www.target.com/p/nintendo-switch-joy-con-l-r-neon-pink-neon-green/-/A-53184864'
-joycons['Joycons: Pink and Green']['links']['Amazon'] = 'B078GZM4H8'
-
-#red and blue
-joycons['Joycons: Red and Blue'] = {}
-joycons['Joycons: Red and Blue']['msrp'] = 79.99
-joycons['Joycons: Red and Blue']['links'] = {}
-joycons['Joycons: Red and Blue']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-red-neon-blue/5730704.p?skuId=5730704'
-joycons['Joycons: Red and Blue']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-blue-red/141870'
-joycons['Joycons: Red and Blue']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Neon-Red-and-Neon-Blue-45496590130/55449978?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=d0e62aa9-f52e-4fb0-3675-5c369b95dac4&config_id=2&customer_id_enc&findingMethod=p13n&guid=d0e62aa9-f52e-4fb0-3675-5c369b95dac4&item_id=55449978&parent_anchor_item_id=656821586&parent_item_id=656821586&placement_id=irs-2-m2&reporter=recommendations&source=new_site&strategy=PWVAV&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
-joycons['Joycons: Red and Blue']['links']['Target'] = 'https://www.target.com/p/nintendo-174-switch-joy-con-153-l-r-ndash-neon-red-neon-blue/-/A-52181032'
-joycons['Joycons: Red and Blue']['links']['Amazon'] = 'B01N6QJ58Y'
-
-#both yellow
-joycons['Joycons: Yellow'] = {}
-joycons['Joycons: Yellow']['msrp'] = 79.99
-joycons['Joycons: Yellow']['links'] = {}
-joycons['Joycons: Yellow']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-yellow/5855105.p?skuId=5855105&cmp=RMX&extStoreId=1091&ref=212&loc=1&gclid=COH-4JecsNoCFRPSswod8A0Hyw&gclsrc=ds'
-joycons['Joycons: Yellow']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-neon-yellow/147371'
-joycons['Joycons: Yellow']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Neon-Yellow-Joy-Con-Pair-L-R-45496590543/55779033?wmlspartner=wlpa&selectedSellerId=1925&adid=22222222227083161155&wmlspartner=wmtlabs&wl0=&wl1=g&wl2=c&wl3=195029111740&wl4=pla-310901747162&wl5=1027461&wl6=&wl7=&wl8=&wl9=pla&wl10=115068521&wl11=online&wl12=55779033&wl13=&veh=sem'
-joycons['Joycons: Yellow']['links']['Target'] = 'https://www.target.com/p/nintendo-174-neon-yellow-joy-conl-r/-/A-52478985'
-joycons['Joycons: Yellow']['links']['Amazon'] = 'B06ZYM3LBP'
-
-#both gray
-joycons['Joycons: Gray'] = {}
-joycons['Joycons: Gray']['msrp'] = 79.99
-joycons['Joycons: Gray']['links'] = {}
-joycons['Joycons: Gray']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-gray/5730705.p?skuId=5730705'
-joycons['Joycons: Gray']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-gray/141855'
-joycons['Joycons: Gray']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Gray-45496590123/55449975'
-joycons['Joycons: Gray']['links']['Target'] = 'https://www.target.com/p/nintendo-174-switch-joy-con-153-l-r-gray/-/A-52180941'
-joycons['Joycons: Gray']['links']['Amazon'] = 'B01N6QKT7H'
-
-#both blue
-joycons['Joycons: Red'] = {}
-joycons['Joycons: Red']['msrp'] = 79.99
-joycons['Joycons: Red']['links'] = {}
-joycons['Joycons: Red']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-blue/5731700.p?skuId=5731700'
-#joycons['Joycons: Red']['Game Stop'] = '
-joycons['Joycons: Red']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-Red/55485016?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=e3566ed5-42b4-49fb-3a5f-52b2a2bdb26a&config_id=106&customer_id_enc&findingMethod=p13n&guid=e3566ed5-42b4-49fb-3a5f-52b2a2bdb26a&item_id=55485016&parent_anchor_item_id=55449975&parent_item_id=55449975&placement_id=irs-106-m3&reporter=recommendations&source=new_site&strategy=PWBAB&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
-joycons['Joycons: Red']['links']['Target'] = 'https://www.target.com/p/joy-con-153-l-r-ndash-neon-red/-/A-52191589'
-joycons['Joycons: Red']['links']['Amazon'] = 'B01MT8USZN'
-
-#both red
-joycons['Joycons: Blue'] = {}
-joycons['Joycons: Blue']['msrp'] = 79.99
-joycons['Joycons: Blue']['links'] = {}
-joycons['Joycons: Blue']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-red/5731708.p?skuId=5731708'
-#joycons['Joycons: Blue']['Game Stop'] = '
-joycons['Joycons: Blue']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Blue-HACAJACAA/55485017?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=4b017346-bc5d-4994-35f3-630e4b72d35c&config_id=2&customer_id_enc&findingMethod=p13n&guid=4b017346-bc5d-4994-35f3-630e4b72d35c&item_id=55485017&parent_anchor_item_id=55485016&parent_item_id=55485016&placement_id=irs-2-m2&reporter=recommendations&source=new_site&strategy=PWVAV&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
-joycons['Joycons: Blue']['links']['Target'] = 'https://www.target.com/p/joy-con-153-l-r-ndash-neon-blue/-/A-52191592'
-joycons['Joycons: Blue']['links']['Amazon'] = 'B01MT8RT5I'
+# joycons = {}
+# #pink and green
+# joycons['Joycons: Pink and Green'] = {}
+# joycons['Joycons: Pink and Green']['msrp'] = 79.99
+# joycons['Joycons: Pink and Green']['links'] = {}
+# joycons['Joycons: Pink and Green']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-pink-neon-green/6182525.p?skuId=6182525'
+# joycons['Joycons: Pink and Green']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-neon-pink-neon-green/157796'
+# joycons['Joycons: Pink and Green']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Neon-Pink-and-Neon-Green-45496881900/656821586'
+# joycons['Joycons: Pink and Green']['links']['Target'] = 'https://www.target.com/p/nintendo-switch-joy-con-l-r-neon-pink-neon-green/-/A-53184864'
+# joycons['Joycons: Pink and Green']['links']['Amazon'] = 'B078GZM4H8'
+#
+# #red and blue
+# joycons['Joycons: Red and Blue'] = {}
+# joycons['Joycons: Red and Blue']['msrp'] = 79.99
+# joycons['Joycons: Red and Blue']['links'] = {}
+# joycons['Joycons: Red and Blue']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-red-neon-blue/5730704.p?skuId=5730704'
+# joycons['Joycons: Red and Blue']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-blue-red/141870'
+# joycons['Joycons: Red and Blue']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Neon-Red-and-Neon-Blue-45496590130/55449978?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=d0e62aa9-f52e-4fb0-3675-5c369b95dac4&config_id=2&customer_id_enc&findingMethod=p13n&guid=d0e62aa9-f52e-4fb0-3675-5c369b95dac4&item_id=55449978&parent_anchor_item_id=656821586&parent_item_id=656821586&placement_id=irs-2-m2&reporter=recommendations&source=new_site&strategy=PWVAV&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
+# joycons['Joycons: Red and Blue']['links']['Target'] = 'https://www.target.com/p/nintendo-174-switch-joy-con-153-l-r-ndash-neon-red-neon-blue/-/A-52181032'
+# joycons['Joycons: Red and Blue']['links']['Amazon'] = 'B01N6QJ58Y'
+#
+# #both yellow
+# joycons['Joycons: Yellow'] = {}
+# joycons['Joycons: Yellow']['msrp'] = 79.99
+# joycons['Joycons: Yellow']['links'] = {}
+# joycons['Joycons: Yellow']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-yellow/5855105.p?skuId=5855105&cmp=RMX&extStoreId=1091&ref=212&loc=1&gclid=COH-4JecsNoCFRPSswod8A0Hyw&gclsrc=ds'
+# joycons['Joycons: Yellow']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-neon-yellow/147371'
+# joycons['Joycons: Yellow']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Neon-Yellow-Joy-Con-Pair-L-R-45496590543/55779033?wmlspartner=wlpa&selectedSellerId=1925&adid=22222222227083161155&wmlspartner=wmtlabs&wl0=&wl1=g&wl2=c&wl3=195029111740&wl4=pla-310901747162&wl5=1027461&wl6=&wl7=&wl8=&wl9=pla&wl10=115068521&wl11=online&wl12=55779033&wl13=&veh=sem'
+# joycons['Joycons: Yellow']['links']['Target'] = 'https://www.target.com/p/nintendo-174-neon-yellow-joy-conl-r/-/A-52478985'
+# joycons['Joycons: Yellow']['links']['Amazon'] = 'B06ZYM3LBP'
+#
+# #both gray
+# joycons['Joycons: Gray'] = {}
+# joycons['Joycons: Gray']['msrp'] = 79.99
+# joycons['Joycons: Gray']['links'] = {}
+# joycons['Joycons: Gray']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-gray/5730705.p?skuId=5730705'
+# joycons['Joycons: Gray']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/accessories/nintendo-switch-joy-con-l-r-gray/141855'
+# joycons['Joycons: Gray']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Gray-45496590123/55449975'
+# joycons['Joycons: Gray']['links']['Target'] = 'https://www.target.com/p/nintendo-174-switch-joy-con-153-l-r-gray/-/A-52180941'
+# joycons['Joycons: Gray']['links']['Amazon'] = 'B01N6QKT7H'
+#
+# #both blue
+# joycons['Joycons: Red'] = {}
+# joycons['Joycons: Red']['msrp'] = 79.99
+# joycons['Joycons: Red']['links'] = {}
+# joycons['Joycons: Red']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-blue/5731700.p?skuId=5731700'
+#
+# joycons['Joycons: Red']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-Red/55485016?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=e3566ed5-42b4-49fb-3a5f-52b2a2bdb26a&config_id=106&customer_id_enc&findingMethod=p13n&guid=e3566ed5-42b4-49fb-3a5f-52b2a2bdb26a&item_id=55485016&parent_anchor_item_id=55449975&parent_item_id=55449975&placement_id=irs-106-m3&reporter=recommendations&source=new_site&strategy=PWBAB&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
+# joycons['Joycons: Red']['links']['Target'] = 'https://www.target.com/p/joy-con-153-l-r-ndash-neon-red/-/A-52191589'
+# joycons['Joycons: Red']['links']['Amazon'] = 'B01MT8USZN'
+#
+# #both red
+# joycons['Joycons: Blue'] = {}
+# joycons['Joycons: Blue']['msrp'] = 79.99
+# joycons['Joycons: Blue']['links'] = {}
+# joycons['Joycons: Blue']['links']['Best Buy'] = 'https://www.bestbuy.com/site/nintendo-joy-con-l-r-wireless-controllers-for-nintendo-switch-neon-red/5731708.p?skuId=5731708'
+#
+# joycons['Joycons: Blue']['links']['Walmart'] = 'https://www.walmart.com/ip/Nintendo-Switch-Joy-Con-Pair-L-R-Blue-HACAJACAA/55485017?action=product_interest&action_type=title&beacon_version=1.0.2&bucket_id=irsbucketdefault&client_guid=4b017346-bc5d-4994-35f3-630e4b72d35c&config_id=2&customer_id_enc&findingMethod=p13n&guid=4b017346-bc5d-4994-35f3-630e4b72d35c&item_id=55485017&parent_anchor_item_id=55485016&parent_item_id=55485016&placement_id=irs-2-m2&reporter=recommendations&source=new_site&strategy=PWVAV&visitor_id=X5Ohiha8GhbLxvAFlzl8pw'
+# joycons['Joycons: Blue']['links']['Target'] = 'https://www.target.com/p/joy-con-153-l-r-ndash-neon-blue/-/A-52191592'
+# joycons['Joycons: Blue']['links']['Amazon'] = 'B01MT8RT5I'
 
 ### games
 games = {}
@@ -101,26 +101,6 @@ games['Splatoon 2']['links']['Target'] = 'https://www.target.com/p/splatoon-2-ni
 games['Splatoon 2']['links']['Amazon'] = 'B01N9QVIRV'
 games['Splatoon 2']['links']['eShop'] = 'https://www.nintendo.com/games/detail/splatoon-2-switch'
 
-games['Rocket League'] = {}
-games['Rocket League']['msrp'] = 19.99
-games['Rocket League']['links'] = {}
-games['Rocket League']['links']['eShop'] = 'https://www.nintendo.com/games/detail/rocket-league-switch'
-
-games['Rocket League Collectors Edition'] = {}
-games['Rocket League Collectors Edition']['msrp'] = 39.99
-games['Rocket League Collectors Edition']['links'] = {}
-# games['Rocket League Collectors Edition']['links']['Best Buy'] = 'https://www.bestbuy.com/site/rocket-league-collectors-edition-nintendo-switch/5896301.p?skuId=5896301&cmp=RMX&extStoreId=1091&ref=212&loc=1&gclid=CJmj79mjstoCFYLXswod7T4MFA&gclsrc=ds'
-# games['Rocket League Collectors Edition']['links']['Game Stop'] = 'https://www.gamestop.com/nintendo-switch/games/rocket-league-collectors-edition/157234'
-# games['Rocket League Collectors Edition']['links']['Walmart'] = 'https://www.walmart.com/ip/Rocket-League-NSW/592506960'
-# #games['Rocket League Collectors Edition']['links']['Target'] =
-# games['Rocket League Collectors Edition']['links']['Amazon'] = 'B077ZQYXY2'
-games['Rocket League Collectors Edition']['links']['eShop'] = 'https://www.nintendo.com/games/detail/rocket-league-collectors-edition-switch'
-
-games['Shovel Knight: Treasure Trove'] = {}
-games['Shovel Knight: Treasure Trove']['msrp'] = 24.99
-games['Shovel Knight: Treasure Trove']['links'] = {}
-games['Shovel Knight: Treasure Trove']['links']['eShop'] = 'https://www.nintendo.com/games/detail/shovel-knight-treasure-trove-switch'
-
 games['Minecraft'] = {}
 games['Minecraft']['msrp'] = 29.99
 games['Minecraft']['links'] = {}
@@ -146,7 +126,14 @@ games['Stardew Valley']['msrp'] = 14.99
 games['Stardew Valley']['links'] = {}
 games['Stardew Valley']['links']['eShop'] = 'https://www.nintendo.com/games/detail/stardew-valley-switch'
 
-# games['Donkey Kong Country: Tropical Freeze'] = {}
-# games['Donkey Kong Country: Tropical Freeze']['msrp'] = 59.99
-# games['Donkey Kong Country: Tropical Freeze']['links'] = {}
-# games['Donkey Kong Country: Tropical Freeze']['links']['eShop'] = 'https://www.nintendo.com/games/detail/donkey-kong-country-tropical-freeze-switch'
+games['Donkey Kong Country: Tropical Freeze'] = {}
+games['Donkey Kong Country: Tropical Freeze']['msrp'] = 59.99
+games['Donkey Kong Country: Tropical Freeze']['links'] = {}
+games['Donkey Kong Country: Tropical Freeze']['links']['eShop'] = 'https://www.nintendo.com/games/detail/donkey-kong-country-tropical-freeze-switch'
+
+games['Sonic Mania'] = {}
+games['Sonic Mania']['msrp'] = 19.99
+games['Sonic Mania']['links'] = {}
+games['Sonic Mania']['links']['eShop'] = 'https://www.nintendo.com/games/detail/sonic-mania-switch'
+
+#https://www.nintendo.com/games/detail/mario-tennis-aces-switch
